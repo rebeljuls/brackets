121e9dd0bb32c1f62dc26d8c004b6184153880d8
rebeljuls <rebel.90@mail.ru>
2023-02-13 16:16:06 +0100

    reat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+ 
+  for (let i=0; i<str.length; i++){
+    if (str.length %2 != 0) {
+      return false;
+    };
+    for (let j = 0; j < i.length; j++){
+      let currentSymbol = str[i][0];
+    }
+    if (bracketsConfig.includes(currentSymbol)){
+      stack.push(currentSymbol);
+       }
+       else {
+        if (stack.length === 0){
+          return false;
+        }
+       
+  
+        let topBrackets = stack[stack.length-1];
+        if (bracketsConfig[currentSymbol] === topBrackets) {
+          stack.pop();
+        }
+        else {
+          return false;
+        }
+       }
+  }
+  return stack.length === 0;
 }
+
