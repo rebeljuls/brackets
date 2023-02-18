bada89713f10a56bb321580f469257f69a3e07e3
rebeljuls <rebel.90@mail.ru>
2023-02-19 00:06:10 +0100

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
  
-  for (let i=0; i<str.length; i++){
-    if (str.length %2 != 0) {
-      return false;
-    };
-  
-   for ( let j = 1; j < bracketsConfig.length; j++){
-         let currentSymbol = str[i];
-  
-     if (bracketsConfig[j][0].includes(currentSymbol)) {
-      stack.push(currentSymbol);
-       }
-       else {
-        if (stack.length === 0){
-          return false;
-        }
-       
-       
-         let pair = bracketsConfig[j][1];
-        let topBrackets = stack[stack.length-1];
-        if (pair[currentSymbol] === topBrackets) {
-          stack.pop();
-        }
-
-         else {
-          return false;
-        }
-        
-
-       }
-  }
  
-  return stack.length === 0;
-}
+  for (let i=0; i<str.length; i++){
+    let currentSymbol = str[i];
+    let find = false;
+      for (let j=0; j < bracketsConfig.length; j ++ ){
+        const openbrackets = bracketsConfig[j][0];
+           //const dictionary = {[bracketsConfig[j][1]]: bracketsConfig[j][0]};
+           
+       
 
+            if (openbrackets.includes(currentSymbol)) {
+                find = true;
+                if (openbrackets == bracketsConfig[j][1]) {
+                    if (stack[stack.length - 1] == openbrackets) {
+                        stack.pop();
+                        break;
+                    }
+                }
+                stack.push(bracketsConfig[j][1]);
+                break;
+            }
+        }
+        if (!find) {
+            if (stack.length == 0 || currentSymbol != stack[stack.length - 1]) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+          
+  
+            
+  return stack.length === 0;
 }
